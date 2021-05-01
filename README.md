12f7e53840b70240c503898a351ed847bb8e7880
muroger <andrz96@mail.ru>
2021-05-01 23:21:27 +0300

    working commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Car plate detection and recognition
+
+For detection [Scaled YoLOv4](https://github.com/WongKinYiu/ScaledYOLOv4/tree/yolov4-large) was used. For recognition - pytesseract.
+
+Every step is in Jupyter Notebook
+
+# Results
+
+![Results](test_batch0_pred.jpg)
+
+# [Dataset](https://drive.google.com/drive/folders/1M7jQFO_m-5cBrvkioDCM3LKOlNllUMqM?usp=sharing)
+
+# TO-DO
+  -   Warp prospective for convenient and more precise recognition
---
+++ utils/detect_plate_number.py
@@ -0,0 +1,93 @@
+import pytesseract
+import cv2
+import re
+
+def recognize_plate(img, coords):
+    # separate coordinates from box
+    xmin, ymin, xmax, ymax = coords
+    #print('coords: ', xmin-5, ymin-5, xmax+5, ymax+5)
+    # get the subimage that makes up the bounded region and take an additional 5 pixels on each side
+    #print(xmin, ymin, xmax, ymax)
+    box = img[int(ymin):int(ymax), int(xmin):int(xmax), :]
+    # grayscale region within bounding box
+    gray = cv2.cvtColor(box, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("Gray", gray)
+    # cv2.waitKey(0)
+    # resize image to three times as large as original for better readability
+    #gray = cv2.resize(gray, None, fx = 3, fy = 3, interpolation = cv2.INTER_CUBIC)
+    # perform gaussian blur to smoothen image
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    # cv2.imshow("Gray", gray)
+    # cv2.waitKey(0)
+    # threshold the image using Otsus method to preprocess for tesseract
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    #thresh =  cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # cv2.imshow("Otsu Threshold", thresh)
+    # cv2.waitKey(0)
+    # create rectangular kernel for dilation
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    # apply dilation to make regions more clear
+    dilation = cv2.dilate(thresh, rect_kern, iterations = 1)
+    # cv2.imshow("Dilation", dilation)
+    # cv2.waitKey(0)
+    # find contours of regions of interest within license plate
+    try:
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #print('OKKK')
+    except:
+        ret_img, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #print('not OK')
+    # sort contours left-to-right
+    sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+    # create copy of gray image
+    im2 = gray.copy()
+    # cv2.imshow("Gray", im2)
+    # cv2.waitKey(0)
+    # create blank string to hold license plate number
+    plate_num = ""
+    # loop through contours and find individual letters and numbers in license plate
+    for cnt in sorted_contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        height, width = im2.shape
+        # if height of box is not tall enough relative to total height then skip
+        if height / float(h) > 6: continue
+
+        ratio = h / float(w)
+        # if height to width ratio is less than 1.5 skip
+        if ratio < 1.5: continue
+
+        # if width is not wide enough relative to total width then skip
+        if width / float(w) > 15: continue
+
+        area = h * w
+        # if area is less than 100 pixels skip
+        if area < 100: continue
+
+        # draw the rectangle
+        rect = cv2.rectangle(im2, (x,y), (x+w, y+h), (0,255,0),2)
+        # grab character region of image
+        roi = thresh[y-5:y+h+5, x-5:x+w+5]
+        # perfrom bitwise not to flip image to black text on white background
+        roi = cv2.bitwise_not(roi)
+        # perform another blur on character region
+        roi = cv2.medianBlur(roi, 5)
+        # print(roi.shape)
+        # cv2.imshow('roi', roi)
+        # cv2.waitKey(0)
+        try:
+            text = pytesseract.image_to_string(roi, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3')
+            # clean tesseract text by removing any unwanted blank spaces
+            #print(text)
+            clean_text = re.sub('\W+','', text)
+            #print(clean_text)
+            #print('clean_text:' ,clean_text)
+            plate_num += clean_text
+        except: 
+            text = None
+
+    if plate_num != None:
+        print("License Plate #: ", plate_num)
+    #cv2.imshow("Character's Segmented", im2)
+    #cv2.waitKey(0)
+    return plate_num
